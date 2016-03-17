a9d1ae6cc64c3e99f3a56658334ee86137e8ce61
Miroslav Simulcik <miroslav.simulcik@in-solut.com>
2016-03-17 10:41:22 +0100

    Added immutable js and entities store

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
 import counter from './modules/counter';
 import locale from './modules/locale';
+import entities from './modules/entities';
 
 export default combineReducers({
   counter,
   router,
   locale,
+  entities,
 });
